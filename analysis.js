65f829d4114aecffdf70ded6c14fa0bcb53b7da7
Stuart <gravatar@strudel.org.uk>
2016-06-06 11:15:14 +0100

    Move number to description

--- analysis.js
+++ analysis.js
@@ -113,8 +113,8 @@ function drawResults(){
 	// For each organisation type
 	for(var t in orgconfig){
 		// By role
-		str += '<h2>'+orgconfig[t].plural.substr(0,1).toUpperCase()+orgconfig[t].plural.substr(1)+' ('+types[t]+')</h2>';
-		str += '<p>The <a href="https://github.com/odileeds/northernpowerhousepeople/blob/master/organisations.csv" class="highlight">data set</a> consists of the following '+orgconfig[t].plural+': '+list[t]+'.</p>';
+		str += '<h2>'+orgconfig[t].plural.substr(0,1).toUpperCase()+orgconfig[t].plural.substr(1)+'</h2>';
+		str += '<p>The <a href="https://github.com/odileeds/northernpowerhousepeople/blob/master/organisations.csv" class="highlight">data set</a> consists of '+types[t]+' '+orgconfig[t].plural+': '+list[t]+'.</p>';
 		str += genderSplitByRole(t,['Leader','Deputy Leader','Elected Mayor','Mayor','Deputy Mayor','Chair','Deputy Chair','Vice Chair','Chief Executive','Deputy Chief Executive','Chief Operating Officer']);
 	}
 
